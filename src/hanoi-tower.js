64d8a79f622d4ec93d989cb52b1c792f2425d6f5
JackNightsky <jack.shapyalevich@gmail.com>
2020-02-20 16:25:09 +0300

    feat: add solution hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,5 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    return {turns: 2**disksNumber-1, seconds: (2**disksNumber-1) / (turnsSpeed/3600) };
+    // throw 'Not implemented';
     // remove line with error and write your code here
 }
