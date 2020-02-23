a024742fb491c9470ab8813e40f13240dfb43472
JackNightsky <jack.shapyalevich@gmail.com>
2020-02-23 21:56:05 +0300

    feat: add solution dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,6 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
+module.exports = function createDreamTeam( members ) {
+  if (!Array.isArray(members)) return false;
+  return members.map(el=> (typeof el) === 'string' ? el.trim()[0].toUpperCase() : '').sort().join("");
+  // throw 'Not implemented';
   // remove line with error and write your code here
 };
